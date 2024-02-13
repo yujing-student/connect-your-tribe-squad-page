a2f7985603b8392252f3b1b61a9e5675fdf30565
yujing <medischdossier03@gmail.com>
2024-02-13 11:11:22 +0100

    er is een indeling gemaakt voor de kaartjes de navigatie moet nog naar de linkerkant

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
-let button = document.querySelector('.button-show-filters');
-let showfirn = document.querySelector('.hide');
-button.addEventListener("click", () => {
-    showfirn.classList.toggle('hide-open');
-    console.log(showfirn+ 'er is geklikt')
-});
+
+click();
+function click(){
+    let button = document.querySelector('.button-show-filters');
+    let hideElements = document.querySelectorAll('.hide');
+    button.addEventListener("click", () => {
+        hideElements.forEach(element => {
+            element.classList.toggle('hide-open');
+        });
+        console.log(hideElements+ 'er is geklikt')
+    });
+}
