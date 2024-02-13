85af301d7074a8329a1167477fa414a9fa841b90
yujing <medischdossier03@gmail.com>
2024-02-13 12:37:55 +0100

    er is bij person informatie toegevoegd daar moet nog naar gekeken worden

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
-
 click();
-function click(){
+
+function click() {
     let button = document.querySelector('.button-show-filters');
     let hideElements = document.querySelectorAll('.hide');
     button.addEventListener("click", () => {
         hideElements.forEach(element => {
             element.classList.toggle('hide-open');
         });
-        console.log(hideElements+ 'er is geklikt')
+        console.log(hideElements + 'er is geklikt')
     });
 }
--- server.js
+++ server.js
@@ -24,36 +24,35 @@ app.use(express.static('public'))
 
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
-  // Haal alle personen uit de WHOIS API op
-  try{/*als de fetch niet lukt om een reden vang de error op en laat het zien zodat een error display word */
-    fetchJson(apiUrl + '/person').then((apiData) => {
-      // apiData bevat gegevens van alle personen uit alle squads
-      // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
+    // Haal alle personen uit de WHOIS API op
+    try {/*als de fetch niet lukt om een reden vang de error op en laat het zien zodat een error display word */
+        fetchJson(apiUrl + '/person').then((apiData) => {
+            // apiData bevat gegevens van alle personen uit alle squads
+            // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
 
-      // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-      response.render('index', {persons: apiData.data, squads: squadData.data})
-      // console.log(squadData.data)
-    })
-  }
-  catch(error){
-    console.log(error)
-  }
+            // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+            response.render('index', {persons: apiData.data, squads: squadData.data})
+            // console.log(squadData.data)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 
 })
 
 // Maak een POST route voor de index
 app.post('/', function (request, response) {
-  // Er is nog geen afhandeling van POST, redirect naar GET op /
-  response.redirect(303, '/')
+    // Er is nog geen afhandeling van POST, redirect naar GET op /
+    response.redirect(303, '/')
 })
 
 // Maak een GET route voor een detailpagina met een request parameter id
 app.get('/person/:id', function (request, response) {
-  // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
-  fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
-    // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
-    response.render('person', {person: apiData.data, squads: squadData.data})
-  })
+    // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
+    fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
+        // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
+        response.render('person', {person: apiData.data, squads: squadData.data})
+    })
 })
 
 // Stel het poortnummer in waar express op moet gaan luisteren
@@ -61,6 +60,6 @@ app.set('port', process.env.PORT || 8000)
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
 })
